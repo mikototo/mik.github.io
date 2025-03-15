abebc890fdbba13be1d3eaf695628a9bfb9eee3c
Michael Kittel <mkittel@uni-muenster.de>
2025-03-15 17:56:48 +0100

    structure

--- Mikato/scripts.js
+++ Mikato/scripts.js
@@ -4,7 +4,7 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const shapes = [];
-const colors = ['#FF00FF', '#00FFFF', '#FFD700'];
+const colors = ['#FF0055', '#00FFAA', '#5500FF', '#FFAA00', '#AAFF00', '#00AAFF'];
 
 function random(min, max) {
     return Math.random() * (max - min) + min;
@@ -14,10 +14,10 @@ class Shape {
     constructor() {
         this.x = random(0, canvas.width);
         this.y = random(0, canvas.height);
-        this.size = random(10, 60);
+        this.size = random(80, 200);
         this.color = colors[Math.floor(random(0, colors.length))];
-        this.speedX = random(-0.5, 0.5);
-        this.speedY = random(-0.5, 0.5);
+        this.speedX = random(-0.3, 0.3);
+        this.speedY = random(-0.3, 0.3);
         this.type = Math.floor(random(0, 3)); // 0:circle, 1:square, 2:triangle
     }
 
@@ -30,7 +30,7 @@ class Shape {
                 ctx.fill();
                 break;
             case 1:
-                ctx.fillRect(this.x, this.y, this.size, this.size);
+                ctx.fillRect(this.x - this.size / 2, this.y - this.size / 2, this.size, this.size);
                 break;
             case 2:
                 ctx.moveTo(this.x, this.y - this.size / 2);
@@ -46,8 +46,10 @@ class Shape {
         this.x += this.speedX;
         this.y += this.speedY;
 
-        if (this.x > canvas.width || this.x < 0) this.speedX *= -1;
-        if (this.y > canvas.height || this.y < 0) this.speedY *= -1;
+        if (this.x > canvas.width + this.size) this.x = -this.size;
+        if (this.x < -this.size) this.x = canvas.width + this.size;
+        if (this.y > canvas.height + this.size) this.y = -this.size;
+        if (this.y < -this.size) this.y = canvas.height + this.size;
     }
 }
 
@@ -71,7 +73,7 @@ window.addEventListener('resize', () => {
     canvas.height = window.innerHeight;
 });
 
-initShapes(40);
+initShapes(25);
 animate();
 
 // Project Upload Function
